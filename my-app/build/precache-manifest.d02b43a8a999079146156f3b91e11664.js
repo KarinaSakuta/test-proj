531b0064bf59ca7d02ace3d09727842c5809064c
Karina <47184215+KarinaSakuta@users.noreply.github.com>
2020-04-03 20:17:43 +0300

    build

---
+++ my-app/build/precache-manifest.d02b43a8a999079146156f3b91e11664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61662c42b4722669f50b191833546725",
+    "url": "/test-proj/index.html"
+  },
+  {
+    "revision": "eff8db900e5e7b64a611",
+    "url": "/test-proj/static/css/main.8ae7677c.chunk.css"
+  },
+  {
+    "revision": "a778bc1471a49d78fe77",
+    "url": "/test-proj/static/js/2.527e5e00.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-proj/static/js/2.527e5e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff8db900e5e7b64a611",
+    "url": "/test-proj/static/js/main.4d571736.chunk.js"
+  },
+  {
+    "revision": "36ab43febef4bf7e6ed2",
+    "url": "/test-proj/static/js/runtime-main.ca57d024.js"
+  }
+]);

0ad7ceb72d8984561a95a8a86a5c28adb43e1211
Karina <47184215+KarinaSakuta@users.noreply.github.com>
2020-04-03 20:05:38 +0300

    Updates

---
+++ precache-manifest.2349669ba70c98a18ccc0d3ef88f6ff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ea2c862eb0cfaabdada2dec7cdceab",
+    "url": "/test-proj/index.html"
+  },
+  {
+    "revision": "a9a32d81e0c37279bb24",
+    "url": "/test-proj/static/css/main.8ae7677c.chunk.css"
+  },
+  {
+    "revision": "b4f89fe77662f6389cf6",
+    "url": "/test-proj/static/js/2.2e22e029.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-proj/static/js/2.2e22e029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a32d81e0c37279bb24",
+    "url": "/test-proj/static/js/main.3a54d070.chunk.js"
+  },
+  {
+    "revision": "0b6d0cfd550769a12c02",
+    "url": "/test-proj/static/js/runtime-main.c57a46fe.js"
+  }
+]);
